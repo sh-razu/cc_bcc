c806bdb9965068d6d1416eb48abe2206c9ebe53c
shahriar.ahmed <shahriar.ahmed@bjitgroup.com>
2025-08-06 13:09:05 +0600

    update: auto populating Cc field with salesperson/buyer id based on the type of quotaion/order

--- models/account_move_send_wizard_ext.py
+++ models/account_move_send_wizard_ext.py
@@ -7,6 +7,30 @@ class AccountMoveSendWizardExt(models.TransientModel):
     cc_email_partner_ids = fields.Many2many('res.partner', 'cc_partners', string='CC Email', )
     bcc_email_partner_ids = fields.Many2many('res.partner', 'bcc_partners', string='CC Email', )
 
+    def default_get(self, fields_list):
+        defaults = super().default_get(fields_list)
+
+        res_ids = (
+                self._context.get('default_res_ids') or
+                self._context.get('default_move_ids') or
+                self._context.get('active_ids')
+        )
+
+        if not res_ids:
+            return defaults
+
+        res_ids = res_ids if isinstance(res_ids, list) else [res_ids]
+        partner_ids = []
+
+        for move in self.env['account.move'].browse(res_ids):
+            if move.invoice_user_id and move.invoice_user_id.partner_id:
+                partner_ids.append(move.invoice_user_id.partner_id.id)
+
+        if partner_ids:
+            defaults['cc_email_partner_ids'] = [(6, 0, list(set(partner_ids)))]
+
+        return defaults
+
     def _get_sending_settings(self):
         settings = super()._get_sending_settings()
         settings['cc_email_partner_ids'] = self.cc_email_partner_ids.ids if self.cc_email_partner_ids else []
